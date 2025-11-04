a622a7b59d9fdcfcfaeb74feb6b723b6db61f6f1
qinbowen98 <97887587+qinbowen98@users.noreply.github.com>
2025-11-04 11:49:12 +0800

    backroll conf.py
    Signed-off-by: qinbowen98 <97887587+qinbowen98@users.noreply.github.com>

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1,28 +1,29 @@
 # Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 import os
 import sys
 from unittest.mock import MagicMock
-
-# -- Path setup --------------------------------------------------------------
-# ✅ 确保 storm 可以被找到
-sys.path.insert(0, os.path.abspath('../..'))
-
-# ✅ Mock 不可安装模块（必须在此位置）
-MOCK_MODULES = ['spams', 'openslide', 'pyvips', 'cv2']
-for mod in MOCK_MODULES:
-    sys.modules[mod] = MagicMock()
-
-
-# -- Project information -----------------------------------------------------
 project = 'STORM'
+copyright = '2025, Zongxu Zhang'
 author = 'Zongxu Zhang'
-copyright = '2025'
+
 
 # -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
+    'sphinx.ext.doctest',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
     'jupyter_sphinx'
 ]
@@ -30,8 +31,21 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = []
 
+napoleon_google_docstring = True
+napoleon_numpy_docstring = False
+doctest_test_doctest_blocks = 'default'
 jupyter_execute_notebooks = "off"
-
+nbsphinx_execute = 'never'
 # -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+sys.path.insert(0, os.path.abspath('../..'))
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+MOCK_MODULES = ['spams','openslide','pyvips','cv2', 'models']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
